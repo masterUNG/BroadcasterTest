4ceb1e2aa4d2749909779a1a1b6a52fd5b7e7c0d
masterUNG <phrombutr@gmail.com>
2016-01-07 13:07:17 +0700

    SplashScreen Finish

--- app/src/main/java/appewtc/masterung/broadcastertest/SplashScreen.java
+++ app/src/main/java/appewtc/masterung/broadcastertest/SplashScreen.java
@@ -1,5 +1,8 @@
 package appewtc.masterung.broadcastertest;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 6000);
+        MediaPlayer introPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_start_horse);
+        introPlayer.start();
+    }   // Main Method
+
+}   // Main Class
