fcf2e4c247f120332387ca7149d4ff1360f6ba56
masterUNG <phrombutr@gmail.com>
2016-01-07 14:28:41 +0700

    Intent From Main ==> DetailListView finish

--- app/src/main/java/appewtc/masterung/broadcastertest/MainActivity.java
+++ app/src/main/java/appewtc/masterung/broadcastertest/MainActivity.java
@@ -1,13 +1,50 @@
 package appewtc.masterung.broadcastertest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private ImageView talkNameImageView, newTestMaleImageView, newTestFemaleImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Image Controller
+        imageController();
+
+    }   // Main Method
+
+    private void imageController() {
+        talkNameImageView.setOnClickListener(this);
+        newTestMaleImageView.setOnClickListener(this);
+        newTestFemaleImageView.setOnClickListener(this);
     }
-}
+
+    private void bindWidget() {
+        talkNameImageView = (ImageView) findViewById(R.id.imageView2);
+        newTestMaleImageView = (ImageView) findViewById(R.id.imageView3);
+        newTestFemaleImageView = (ImageView) findViewById(R.id.imageView4);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+        //Intent to Detail ListView
+        Intent objIntent = new Intent(MainActivity.this, DetailListView.class);
+        startActivity(objIntent);
+
+    }   // onClick
+
+}   // Main Class
